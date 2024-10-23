57141fba56b1fb021c03fc21e0b83f4a0342df34
Giselly031 <00001135975139sp@al.educacao.sp.gov.br>
2024-10-23 19:44:53 -0300

    esse e o ultimo

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é python e para que serve?',
+    'O Python é uma linguagem de programação amplamente usada em aplicações da Web, desenvolvimento de software, ciência de dados e machine learning'
+)
+
+criaCartao(
+    'Geografia',
+    'O que e ONU?',
+    'A Organização das Nações Unidas, também conhecida pela sigla ONU, é uma organização internacional formada por países que se reuniram voluntariamente para trabalhar pela paz e o desenvolvimento mundiais'
+)
+
+criaCartao(
+    'Historia',
+    'qual foi o motivo da guerra mundial?',
+    'Foi resultado de inúmeros fatores, como a rivalidade econômica, ressentimentos por acontecimentos passados e questões nacionalistas'
+)
+
+criaCartao(
+    'Matemática',
+    'qual foi o dia que surgiu a matematica?',
+    'A Matemática, como a conhecemos hoje, surgiu no Antigo Egito e no Império Babilônico, por volta de 3500 a.C. Porém, na pré-história, os seres humanos já usavam os conceitos de contar e medir'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'como se diz "A jovem não gosta de programação" em inglês?',
+    'A jovem não gosta de programação" em inglês é the young woman doesnt like programming'
+)
+
+criaCartao(
+    'Lingua Portugesa',
+    'qual a história de iracema?',
+    'Iracema pertence ao povo tabajara e é a filha virgem do pajé. Martim é aliado dos pitiguaras, inimigos dos tabajaras, e está perdido em território inimigo. O encontro entre os dois dá início a um romance que serve de lenda para contar o surgimento do Estado do Ceará'
+)
